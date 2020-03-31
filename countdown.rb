121ddf185d83f325cd0d53769431be09883b16df
Shamel Lakin <christlife4u@gmail.com>
2020-03-31 13:44:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
-#write your code here
 
-def countdown
+
+def countdown(num)
+  number = 10
+  while number = 0
+    number -= 1
+    puts "#{number} SECOND(S)!"
+  end
+    return "HAPPY NEW YEAR!"
 end
